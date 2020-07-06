98f2ef170f456871e14141023483dc40f07010f1
patrick <patricksinclair@hotmail.co.uk>
2020-07-06 19:07:47 +0100

    improved interpolation script

--- 3D_riblet/interpolater_script.sh
+++ 3D_riblet/interpolater_script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+#MAKE SURE THAT STARTFROM IN THE CONTROLDICT IS SET TO LATEST TIME
+#ALSO MAKE SURE THAT THAT 0/alpha.biofilm IN THE SOURCE DIRECTORY HAS BEEN DELETED
+
 dynamicMeshFilePath='../../d_23_thickBF/3D_riblet/'
-#dynamicMeshTime0='../../d_23_thickBF/3D_riblet/0'
 dynamicMeshTime0="$dynamicMeshFilePath/0"
 
 
 #these next two commands make sure there aren't any time directories in the target case that could
 #interfere with the mapFields utility
 foamListTimes -rm
-rm -r 0
+#rm 0/alpha.biofilm
+setFields
 
 for t in $(seq 0 0.2 15);
 do
@@ -20,9 +23,11 @@ do
   fi
 
   cp -r $dynamicMeshTime0 $t
+  #use sed to change the $t/uniform/time value (optional I think)
+  
 
-  mapFields -consistent -sourceTime $t $dynamicMeshFilePath
-
-
+  mapFields -consistent -sourceTime $t $dynamicMeshFilePath -mapMethod mapNearest
+  
 
 done
+
